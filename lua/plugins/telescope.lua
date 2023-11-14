f92ea6afacf93e0a5d58a008ad945cdf7ff7a2f0
Max Marschall <max.marschall@thinktecture.com>
2023-11-14 15:56:33 +0100

    ohyea

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,41 +1,47 @@
 return {
-    { "nvim-telescope/telescope-fzf-native.nvim" },
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
-            config = function()
-                require("telescope").load_extension("fzf")
-            end,
-        },
-        opts = {
-            defaults = {
-                lsp_document_symbols = {
-                    fname_width = 160,
-                    show_line = true,
-                    symbol_width = 160,
-                },
-                dynamic_preview_title = true,
-                path_display = {
-                    smart = "true",
-                    shorten = {
-                        len = 3,
-                        exclude = { 1, -1 },
-                    },
-                    truncate = true,
-                },
-                show_line = true,
-                fname_width = 160,
-                layout_strategy = "vertical",
-                layout_config = {
-                    prompt_position = "top",
-                    vertical = { width = 0.9 },
-                    horizontal = { width = 0.9 },
-                },
-                sorting_strategy = "ascending",
-                winblend = 0,
-            },
-        },
+  { "nvim-telescope/telescope-fzf-native.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
     },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>",                                            desc = "Find Files" },
+      { "<leader>f/", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Grep string" },
+
+    },
+    opts = {
+      defaults = {
+        lsp_document_symbols = {
+          fname_width = 160,
+          show_line = true,
+          symbol_width = 160,
+        },
+        dynamic_preview_title = true,
+        path_display = {
+          smart = "true",
+          shorten = {
+            len = 3,
+            exclude = { 1, -1 },
+          },
+          truncate = true,
+        },
+        show_line = true,
+        fname_width = 160,
+        layout_strategy = "vertical",
+        layout_config = {
+          prompt_position = "top",
+          vertical = { width = 0.9 },
+          horizontal = { width = 0.9 },
+        },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
 }
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,8 +1,8 @@
 return {
-    {
-        "mbbill/undotree",
-        keys = {
-            { "<leader>U", "<cmd>UndotreeToggle<cr>", { descr = "[U]ndoTree Toggle" } },
-        },
+  {
+    "mbbill/undotree",
+    keys = {
+      { "<leader>U", "<cmd>UndotreeToggle<cr>", { descr = "[U]ndoTree Toggle" } },
     },
+  },
 }
