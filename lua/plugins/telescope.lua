1c5e4fda6cf9f0c4be736e2848e15e2d3b779ffa
Max Marschall <max.marschall@thinktecture.com>
2023-12-05 07:08:32 +0100

    lockfile

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,14 @@
 return {
-  { "nvim-telescope/telescope-fzf-native.nvim" },
+
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-telescope/telescope-live-grep-args.nvim",
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'CFLAGS=-march=native make',
+        lazy = true
+      },
     },
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>",                                            desc = "Find Files" },
@@ -16,6 +16,16 @@ return {
 
     },
     opts = {
+      extensions_list = { "fzf" },
+      extensions = {
+        fzf = {
+          fuzzy = true,                   -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true,    -- override the file sorter
+          case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        }
+      },
       defaults = {
         lsp_document_symbols = {
           fname_width = 160,
